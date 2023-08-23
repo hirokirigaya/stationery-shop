b8a3eed2431d20758908393356476ce5f4064dd8
hirokirigaya <privz286@gmail.com>
2023-08-22 23:28:49 -0300

    fix(types): fixed productType

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,8 +1,8 @@
-export type ProductType = {
-  _id: '64e41dcccbdab4c21fd1458a';
-  name: 'Product 1';
-  price: 120;
-  description: 'lorem psuim';
-  category: 'lorem';
-  __v: 0;
-};
+export interface ProductType {
+  _id: string;
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+  __v: number;
+}
